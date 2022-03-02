4154c2577041cb8325b00908cc1389d4d1477ec2
SamikEshti <ahmedsamik97@gmai.com>
2022-03-02 18:27:56 +0600

    add details 2

--- js/phone.js
+++ js/phone.js
@@ -39,7 +39,7 @@ const displaySearchResult = phones => {
             <div class="card-body">
                 <h5 class="card-title">${mobile.phone_name}</h5>
                 <p class="card-text">${mobile.brand}</p>
-                <button onclick="loadPhoneDetail('${mobile.brand}')" class="btn btn-primary" type="button"
+            <button onclick="loadPhoneDetail('${mobile.slug}')" class="btn btn-primary" type="button"
             id="detail">detail</button>
             </div>
         </div>
@@ -61,10 +61,22 @@ const displayPhoneDetail = mobile => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <img src="${mobile.image}" class="card-img-top" alt="...">
+    <img src="${mobile.image}" class="card-img-top w-50 mx-auto" alt="...">
     <div class="card-body">
-        <h5 class="card-title">${mobile.brand}</h5>
-        <p class="card-text">${mobile.slug}</p>
+    <h4 class="text-center"><em><u> Specifiations </u></em></h4> <br>
+        <p class="card-text">Brand : ${mobile.brand}</p>
+        <p class="card-text">Phone Name : ${mobile.name}</p>
+        <p class="card-text">Storage : ${mobile.mainFeatures.storage}</p>
+        <p class="card-text">Display Size : ${mobile.mainFeatures.displaySize}</p>
+        <p class="card-text">Chip Set : ${mobile.mainFeatures.chipSet}</p>
+        <p class="card-text">Release Date : ${mobile.releaseDate}</p> <br>
+
+        <p class="card-text">WLAN : ${mobile.others.WLAN}</p>
+        <p class="card-text">Bluetooth : ${mobile.others.Bluetooth}</p>
+        <p class="card-text">GPS: ${mobile.others.GPS}</p>
+        <p class="card-text">NFC : ${mobile.others.NFC}</p>
+        <p class="card-text">Radio : ${mobile.others.Radio}</p>
+        <p class="card-text">USB : ${mobile.others.USB}</p>
     </div>
     `;
     phoneDetails.appendChild(div);
